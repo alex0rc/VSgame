27cad69294ffb15a77b4bee16ea866ef7d20bd71
jordiForn <thejfk18@gmail.com>
2025-11-21 12:26:26 +0100

    Login fixed

--- api/login.php
+++ api/login.php
@@ -7,18 +7,22 @@ use admin\models\Database;
 require_once '../../admin/models/User.php';
 require_once '../../admin/models/Database.php';
 
+session_start();
+
 $db = Database::getInstance();
 $con = $db->connect();
 
 $username = $_POST['username'] ?? null;
 $password = $_POST['password'] ?? null;
 
-$user = new User()->getByUsername($username);
+$u = new User();
+$user = $u->getByUsername($username);
 
 if($user){
-    if($user->getPassword() == password_hash($password, PASSWORD_DEFAULT)){
+    if (password_verify($password, $user->getPassword())) {
         $_SESSION['user'] = $user;
         header('Location: ../views/show.php');
+        exit;
     }else{
         echo "Error: Contraseña incorrecta";
     }
