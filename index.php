12e7e9deda88473eda97366d0819fbd862bc8a3b
jordiForn <thejfk18@gmail.com>
2025-11-27 11:08:01 +0100

    BE: Router listo para imagenes

--- index.php
+++ index.php
@@ -38,4 +38,36 @@ if (file_exists($controllerFile)) {
     echo "Error: Archivo '$controllerFile' no encontrado.";
 }
 
+// Servir imágenes desde /assets/img
+if (isset($_GET['image'])) {
+    $imageName = basename($_GET['image']);
+    $folder = 'img';
+
+    // Si empieza con 'cards/', se sirve desde la subcarpeta cards
+    if (str_starts_with($imageName, 'cards/')) {
+        $folder = 'img/cards';
+        $imageName = substr($imageName, 6);
+    }
+
+    $imagePath = __DIR__ . "/assets/$folder/" . $imageName;
+
+    if (file_exists($imagePath)) {
+        $ext = pathinfo($imagePath, PATHINFO_EXTENSION);
+        switch (strtolower($ext)) {
+            case 'jpg':
+            case 'jpeg': header('Content-Type: image/jpeg'); break;
+            case 'png': header('Content-Type: image/png'); break;
+            case 'gif': header('Content-Type: image/gif'); break;
+            default: header('Content-Type: application/octet-stream');
+        }
+        readfile($imagePath);
+        exit;
+    } else {
+        http_response_code(404);
+        echo "Imagen no encontrada.";
+        exit;
+    }
+}
+
+
 ?>
